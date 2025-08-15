7595272ba32eb23cb4102cd0f0e95dab56340686
Sree Vaishnavi Chatrati <sreevaishnavichatrati@gmail.com>
2025-08-15 16:59:07 +0530

    Update Armstrong.java

--- Day1/ArmstrongCheck.java
+++ Day1/ArmstrongCheck.java
@@ -5,11 +5,18 @@ public class ArmstrongNumber {
         System.out.print("Enter a number: ");
         int num = sc.nextInt();
         int original = num;
+        int temp = num;
         int sum = 0;
-        while (num > 0) {
-            int digit = num % 10;
-            sum += digit * digit * digit;
-            num /= 10;
+        int count = 0;
+        while (temp > 0) {
+            temp /= 10;
+            count++;
+        }
+        temp = num; 
+        while (temp > 0) {
+            int digit = temp % 10;
+            sum += Math.pow(digit, count);
+            temp /= 10;
         }
         if (sum == original) {
             System.out.println(original + " is an Armstrong number.");
